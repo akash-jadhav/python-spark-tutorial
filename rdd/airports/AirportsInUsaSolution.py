fb6f447c64b57645b778fdccb193c04e39a447b4
zs\aj13566 <akash.a.jadhav@zs.com>
2018-10-02 16:56:52 +0530

    Changed something

--- rdd/airports/AirportsInUsaSolution.py
+++ rdd/airports/AirportsInUsaSolution.py
@@ -1,10 +1,11 @@
 import sys
 sys.path.insert(0, '.')
 from pyspark import SparkContext, SparkConf
-from commons.Utils import Utils
+import re
 
-def splitComma(line: str):
-    splits = Utils.COMMA_DELIMITER.split(line)
+def splitComma(line):
+    COMMA_DELIMITER = re.compile(''',(?=(?:[^"]*"[^"]*")*[^"]*$)''')
+    splits = COMMA_DELIMITER.split(line)
     return "{}, {}".format(splits[1], splits[2])
 
 if __name__ == "__main__":
@@ -12,7 +13,8 @@ if __name__ == "__main__":
     sc = SparkContext(conf = conf)
 
     airports = sc.textFile("in/airports.text")
-    airportsInUSA = airports.filter(lambda line : Utils.COMMA_DELIMITER.split(line)[3] == "\"United States\"")
+    COMMA_DELIMITER = re.compile(''',(?=(?:[^"]*"[^"]*")*[^"]*$)''')
+    airportsInUSA = airports.filter(lambda line : COMMA_DELIMITER.split(line)[3] == "\"United States\"")
 
     airportsNameAndCityNames = airportsInUSA.map(splitComma)
     airportsNameAndCityNames.saveAsTextFile("out/airports_in_usa.text")
